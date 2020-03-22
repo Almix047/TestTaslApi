42b7ba4cd56ba5a58ffd569e6eb8a2878ee8cf9b
Almix047 <Amix047@gmail.com>
2020-03-22 13:31:47 +0300

    Added post rating

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::RatingsController < ApiController
+  def create
+    @rating = Rating.new(rating_params.merge(post_id: params['post_id']))
+
+    if @rating.save
+      render json: { status: 200, avg_rating: Post.find(params['post_id']).rating.average(:point).to_f }, status: :ok
+    else
+      render json: { status: 422, message: @rating.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:point)
+  end
 end
