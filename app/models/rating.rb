9bd4c5c1c3a78d2b8e297bd064e890ec209ca340
Almix047 <Amix047@gmail.com>
2020-03-27 16:12:08 +0300

    Added validation of rating value

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,5 @@
 class Rating < ApplicationRecord
   belongs_to :post
+
+  validates :point, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: 'Only integers from 1 to 5'}
 end
